0a6d514a60270e8b3a239ef9f1be8e67f3952e64
Arielv1 <arielv1994@gmail.com>
2020-09-23 16:40:59 +0300

    Added OnItemClick interface - better implementation

--- app/src/main/java/com/example/running/ListCardAdapter.java
+++ app/src/main/java/com/example/running/ListCardAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
@@ -19,11 +20,19 @@ import java.util.Arrays;
 
 public class ListCardAdapter extends RecyclerView.Adapter <RecyclerView.ViewHolder> {
 
-    class ListViewHolder extends RecyclerView.ViewHolder {
+
+    interface OnItemClickListener {
+        void onItemEdit(int position);
+        void onItemDelete(int position);
+    }
+
+
+
+    class ListViewHolder extends RecyclerView.ViewHolder implements  OnClickListener{
 
         TextView mDate, mActivityType, mPace, mDuration, mDistance, mEdit, mDelete;
-
-        public ListViewHolder(@NonNull View itemView) {
+        OnItemClickListener listener;
+        public ListViewHolder(@NonNull View itemView, final OnItemClickListener listener) {
             super(itemView);
             mDate = itemView.findViewById(R.id.history_list_LBL_date);
             mActivityType = itemView.findViewById(R.id.history_list_LBL_type);
@@ -32,6 +41,36 @@ public class ListCardAdapter extends RecyclerView.Adapter <RecyclerView.ViewHold
             mDistance = itemView.findViewById(R.id.history_list_LBL_km);
             mEdit= itemView.findViewById(R.id.history_list_LBL_edit);
             mDelete= itemView.findViewById(R.id.history_list_LBL_delete);
+            itemView.setOnClickListener(this);
+
+            mEdit.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemEdit(position);
+                        }
+                    }
+                }
+            });
+
+            mDelete.setOnClickListener(new OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    if (listener != null){
+                        int position = getAdapterPosition();
+                        if (position != RecyclerView.NO_POSITION){
+                            listener.onItemDelete(position);
+                        }
+                    }
+                }
+            });
+        }
+
+        @Override
+        public void onClick(View v) {
+
         }
     }
 
@@ -48,20 +87,24 @@ public class ListCardAdapter extends RecyclerView.Adapter <RecyclerView.ViewHold
         }
     }
 
-
-
     private ArrayList<CardioActivity> cardioActivities;
     private int viewTypeRequset;
     private Context context;
-
+    private OnItemClickListener mListener;
     public ListCardAdapter(Context context){
         this.context=context;
     }
 
-    public ListCardAdapter(ArrayList<CardioActivity> cardioActivities, int viewTypeRequset, Context context) {
+    public ListCardAdapter(ArrayList<CardioActivity> cardioActivities, OnItemClickListener listener, int viewTypeRequset) {
+        this.context = context;
+        this.mListener = listener;
         this.cardioActivities = cardioActivities;
         this.viewTypeRequset = viewTypeRequset;
-        this.context = context;
+
+    }
+
+    public void setOnItemClickListener(OnItemClickListener onItemClickListener){
+        this.mListener = onItemClickListener;
     }
 
     public void setCardioActivities(ArrayList<CardioActivity> cardioActivities) {
@@ -79,7 +122,7 @@ public class ListCardAdapter extends RecyclerView.Adapter <RecyclerView.ViewHold
         switch (viewTypeRequset) {
             case Utils.AdapterViewOptions.LIST:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.history_list, parent, false);
-                return new ListViewHolder(view);
+                return new ListViewHolder(view, mListener);
             case Utils.AdapterViewOptions.CARD:
                 view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_log_card, parent, false);
                 return new CardViewHodler(view);
@@ -101,27 +144,6 @@ public class ListCardAdapter extends RecyclerView.Adapter <RecyclerView.ViewHold
                 ((ListViewHolder) holder).mDistance.setText(cardioActivity.getDistance()+"");
                 ((ListViewHolder) holder).mPace.setText(cardioActivity.getPace()+"");
                 ((ListViewHolder) holder).mDuration.setText(cardioActivity.getDuration()+"");
-                ((ListViewHolder) holder).mEdit.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent intent = new Intent(context, Activity_Add_Manually.class);
-                        intent.putExtra(Keys.NEW_DATA_PACKAGE, cardioActivity);
-                        context.startActivity(intent);
-                    }
-                });
-                ((ListViewHolder) holder).mDelete.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        cardioActivities.remove(cardioActivity);
-                        notifyDataSetChanged();
-
-                        Gson gson = new Gson();
-                        AllSportActivities allSportActivities = gson.fromJson(MySP.getInstance()
-                                .getString(Keys.ALL_CARDIO_ACTIVITIES, Keys.DEFAULT_ALL_CARDIO_ACTIVITIES_VALUE), AllSportActivities.class);
-                        allSportActivities.setActivities(cardioActivities);
-                        MySP.getInstance().putString(Keys.ALL_CARDIO_ACTIVITIES, gson.toJson(allSportActivities));
-                    }
-                });
                break;
             case Utils.AdapterViewOptions.CARD:
                 ((CardViewHodler)holder).upper_text.setText("Changed Upper Text");
