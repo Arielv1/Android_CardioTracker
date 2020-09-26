00c64203a12ab3d541b7a4dcac3dc02f6b7a3416
Arielv1 <arielv1994@gmail.com>
2020-09-26 20:18:18 +0300

    Finished History logic, set up New_Record GPS

---
+++ app/src/main/java/com/example/running/FirebaseInitializer.java
@@ -0,0 +1,50 @@
+package com.example.running;
+
+import android.content.Context;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+
+public class FirebaseInitializer {
+
+    private static final String TAG = "MyFB";
+    private static FirebaseInitializer instance;
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
+
+
+    public static FirebaseInitializer getInstance() {
+        return instance;
+    }
+
+    private FirebaseInitializer(Context context, String email, String password) {
+
+        FirebaseApp.initializeApp(context);
+        FirebaseAuth.getInstance().signInWithEmailAndPassword(
+                email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(Task<AuthResult> task) {
+                //If the user has been authenticated...//
+                if (task.isSuccessful()) {
+                    Log.d(TAG, "Firebase authentication Successful sign in");
+                } else {
+                    //If sign in fails, then log the error//
+                    Log.d(TAG, "Firebase authentication failed");
+                }
+            }
+        });
+        database = FirebaseDatabase.getInstance();
+    }
+
+    public static FirebaseInitializer init(Context context, String email, String password) {
+        if (instance == null)
+            instance = new FirebaseInitializer(context, email, password);
+        return instance;
+    }
+}
